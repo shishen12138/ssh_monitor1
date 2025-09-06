d7f3ad289f62cf624469e9b4ee978b8f3f9448a5
Sscloud <93865885+shishen12138@users.noreply.github.com>
2025-09-06 13:03:21 +0800

    更新 1.sh

--- 1.sh
+++ 1.sh
@@ -3,7 +3,7 @@
 set -e
 
 WORKDIR="/root/ssh_monitor"
-REPO_URL="https://github.com/shishen12138/ssh_monitor1/blob/main"
+REPO_URL="wget https://raw.githubusercontent.com/shishen12138/ssh_monitor1/main/"
 
 echo "=== 安装系统依赖 ==="
 apt update
