4fcce93bc37da8e228b2716920c6e346b4cab826
Sscloud <93865885+shishen12138@users.noreply.github.com>
2025-09-06 14:01:54 +0800

    更新 start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-source /opt/ssh_monitor/venv/bin/activate
-exec uvicorn main:app --host 0.0.0.0 --port 8000
+# 启动 SSH Monitor Web 面板
+
+# 绝对路径调用虚拟环境中的 uvicorn
+/root/ssh_monitor/venv/bin/uvicorn main:app --host 0.0.0.0 --port 12138
